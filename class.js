308fd4ccd4de3b6d560ff7385fe7beb102013e03
maggie serrano <https://github.com/magser110/October2024.git>
2025-01-16 14:38:39 -0500

    class exercises done

---
+++ class.js
@@ -0,0 +1,23 @@
+// data types = specific typs of info
+// strings, booleans, numbers/integars
+
+console.log(900);
+console.log("my fav movie is lilo and stitch");
+console.log(true, false);
+
+// math operators
+// subtraction
+console.log(5-5);
+//  addition
+console.log(5 + 5);
+//  division
+console.log(5 / 5);
+//  multiplication
+console.log(5 * 5);
+
+// variables 
+// let, const, var
+var globalVariable = 100;
+console.log(globalVariable);
+let localVariable = 100; // variable can be changed
+const constantVariable = 100; // variable cannot be changed
